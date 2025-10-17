a4100b519cafa70ce301988d7b2076d43993cac7
Pouria Tooranjipour <pouriatooranjipour@gmail.com>
2025-10-16 23:03:13 -0400

    remove log data

--- build/controller/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/pouria/Documents/Lidar_based_MPC_CBF_github/LiDAR_based-MPC_CBF/install/controller'
